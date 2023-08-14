a2b06e3bfa53d2560486219fd529348e8c0a2b3a
Q. Zhong <12ff54e@gmail.com>
2023-08-14 12:12:31 +0800

    Add key to list item in gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -3,21 +3,24 @@ import styles from './gallery.module.css';
 import Image from 'next/image';
 
 export default function Gallery(props: { src: string[] }) {
-    const createListItem = (link: string) => (
-        <li className={styles.slide}>
-            <a className={styles.link} href="#">
-                <Image
-                    className={styles.img}
-                    src={link}
-                    width={500}
-                    height={500}
-                    alt={path
-                        .basename(link)
-                        .slice(0, -path.extname(link).length)}
-                />
-            </a>
-        </li>
-    );
+    const createListItem = (link: string) => {
+        const baseName = path
+            .basename(link)
+            .slice(0, -path.extname(link).length);
+        return (
+            <li className={styles.slide} key={baseName}>
+                <a className={styles.link} href="#">
+                    <Image
+                        className={styles.img}
+                        src={link}
+                        width={500}
+                        height={500}
+                        alt={baseName}
+                    />
+                </a>
+            </li>
+        );
+    };
 
     return (
         <div className={styles.container}>
